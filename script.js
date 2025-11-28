4362ca73112c4d3b68702eb28a22ee0e72b2afe7
Иван <sloboshevich@bk.ru>
2025-11-28 16:53:36 +0300

    feat(js): подключить script.js и добавить примеры с переменными

---
+++ script.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = 5;
+let sum = a + b;
+let difference = a - b;
+let product = a * b;
+let quetient = a / b;
+let firstName = "Анна";
+let lastName = "Иванова";
+let fullName = firstName + " " + lastName;
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quetient);
+console.log(fullName);
